7a72993aae750e86f9cdcf4baa69fa76b21f9237
Enzo Patti <enzo.patti@aluno.senai.br>
2024-11-10 05:22:07 -0300

    componentes organizados

---
+++ src/components/Value.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, View, Text } from "react-native";
+type ValueProps = {
+ value: string;
+ label: string;
+};
+const Value = ({ label, value }: ValueProps) => (
+ <View>
+  <Text style={styles.label}>{label}</Text>
+  <Text style={styles.value}>{value}</Text>
+ </View>
+);
+
+const styles = StyleSheet.create({
+ label: {
+  color: "white",
+  fontSize: 20,
+ },
+ value: {
+  fontSize: 45,
+  color: "#AFB3BE",
+  fontWeight: 500,
+ },
+});
+
+export default Value;
